217311e850f8b8b1bdfe1e6885422a9bf6458127
Ian Johnson <ijohnson@wolfram.com>
2015-08-25 09:39:01 -0500

    More stuff fixing nonsense

--- JavaApplication31/src/javaapplication31/JavaApplication31.java
+++ JavaApplication31/src/javaapplication31/JavaApplication31.java
@@ -19,5 +19,8 @@ public class JavaApplication31 {
     }
     
 	asd'fsadjlsadfkjsadjklsadfjkl
+                
+                
+                sdafkljasdljkafsdkljasfdkljasdfj;klasdfjkl;asdfklj;saf
 	
 }
